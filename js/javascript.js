456fb83b55398a0062609fcb32e99a2dc16d6ef8
sunakshi96 <sunakshitejwani@gmail.com>
2017-07-16 23:31:03 +0530

    toggle between fahrenheit and celsius temperature

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,9 @@
 	$(document).ready(function(){
 		var lon;
 		var lat;
+		var ftemp;
+		var kelvin;
+		var ctemp;
 		if(navigator.geolocation){
 			navigator.geolocation.getCurrentPosition(function(position){
 				lon=position.coords.longitude;
@@ -10,11 +13,32 @@
 				  $.getJSON(api,function(data){
     //alert(data.coord.lat);
     				var weatherType=data.weather[0].description;
-    				var kelvin = data.main.temp;
+    				kelvin = data.main.temp;
     				var windSpeed = data.wind.speed;
     				var city = data.name;
-    				console.log(city);
-    				console.log(api);
+    				//console.log(city);
+    				//console.log(api);
+    				ftemp = (kelvin)*(9/5)+459.62;
+    				ctemp = kelvin - 273;	
+    				console.log(ctemp);
+    				console.log(ftemp);
+    				$("#city").html(city);
+    				$("#weatherType").html(weatherType);
+    				$("#fTemp").html(ftemp);
+    				$("#windSpeed").html(windSpeed);
+    				var tempSwap=false;	
+    				$("#fTemp").click(function(){
+    					if(tempSwap===false){
+    						$("#fTemp").html(ctemp);
+    						tempSwap=true;
+
+    					}
+    					else{
+    						$("#fTemp").html(ftemp);
+    						tempSwap=false;
+    					}
+    				});
+
 				  });
 			});
 		}
