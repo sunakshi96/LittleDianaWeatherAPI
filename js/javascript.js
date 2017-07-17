1dfb0b7baa9f48ab572f68662327d9d3e975d13b
sunakshi96 <sunakshitejwani@gmail.com>
2017-07-17 12:58:48 +0530

    modify background image with temperature

--- js/javascript.js
+++ js/javascript.js
@@ -18,23 +18,36 @@
     				var city = data.name;
     				//console.log(city);
     				//console.log(api);
-    				ftemp = (kelvin)*(9/5)+459.62;
-    				ctemp = kelvin - 273;	
+    				ftemp = ((kelvin)*(9/5)+459.62).toFixed(2);
+    				ctemp = (kelvin - 273).toFixed(2);	
     				console.log(ctemp);
     				console.log(ftemp);
     				$("#city").html(city);
     				$("#weatherType").html(weatherType);
-    				$("#fTemp").html(ftemp);
-    				$("#windSpeed").html(windSpeed);
-    				var tempSwap=false;	
+    				$("#fTemp").html(ftemp + ' &#8457;');
+    				windSpeed = (2.237 * (windSpeed)).toFixed(1);
+    				$("#windSpeed").html(windSpeed + ' mph');
+    				var tempSwap=false;
+    				if(fTemp>80){
+     $('body').css('background-image','url(../images/summer.jpeg)');
+    }
+     else if(fTemp>70){
+         $('body').css('background-image','url(../images/clouds-cloudporn-weather-lookup-158163.jpeg)');
+     }
+       else if(fTemp>60){
+         $('body').css('background-image','url(../images/rain.jpeg)');
+     }
+      else if(fTemp>50){
+        $('body').css('background-image','url(../images/winter.jpeg)');
+      } 
     				$("#fTemp").click(function(){
     					if(tempSwap===false){
-    						$("#fTemp").html(ctemp);
+    						$("#fTemp").html(ctemp + ' &#8451;');
     						tempSwap=true;
 
     					}
     					else{
-    						$("#fTemp").html(ftemp);
+    						$("#fTemp").html(ftemp + ' &#8457;');
     						tempSwap=false;
     					}
     				});
